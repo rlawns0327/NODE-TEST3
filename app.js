3433c3850a4f579569a34f16557a11a8288d538f
rlawns0327 <30451538+rlawns0327@users.noreply.github.com>
2019-08-07 22:38:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = http.createServer(function(request,response){
         var list = template.list(topics);
         var html = template.HTML(title, list,
           
-          `<h2>${title}</h2>${description}<iframe src="http://113.198.235.227:8081" width="800" height = "510"> </iframe>`,
+          `<h2>${title}</h2>${description}<iframe src="http://113.198.235.227:8081" width="800" height = "510"> </iframe></iframe><embed height="500" width="700" src="http://www.gagalive.kr/livechat1.swf?chatroom=김준방"></embed>`,
           `<a href="/create">create</a>`
         );
         response.writeHead(200);
